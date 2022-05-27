3d04094c7e4ef8b6c71a9d847cf5a8ae2a7b0987
Natalie Wagner <nrwagner723@gmail.com>
2022-05-26 19:27:56 -0500

    created all files and class names

---
+++ main.py
@@ -0,0 +1,15 @@
+# step 1: display the rules of the game
+# step 2: ask how many humans are playing 
+# step 3: display each gesture option 
+
+# build game class steps 1-3 and have methods for rules, asking how many humans, and displaying gesture. then run_game method which calls all the other ones 
+
+# step 4: ask the humans which gesture they want to pick
+# step 5: display what each player and/or AI chose for their gesture 
+# step 6: display who won the round or if it was a tie
+# step 7: repeat steps 3 - 6 until someone has 2 wins
+# step 8: display the winner of the game
+
+# player base class and an AI class
+# if theres 2 players, they each inherit from player base class which stores the logic for asking what gesture they want to pick
+# if theres a player and an AI the player inherits from player base class and and the ai is from separate class that randomizes gesture choice. 
