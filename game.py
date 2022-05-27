9a7de1f807b1880aa1a56023b0c8b2c5911b8e67
Natalie Wagner <nrwagner723@gmail.com>
2022-05-26 20:51:51 -0500

    added a place in the player class to store each gesture

--- game.py
+++ game.py
@@ -31,24 +31,27 @@ class Game:
         print('Rock crushes Scissors')
 
     def how_many_humans(self):
-        number_of_players = input('How many players? Enter [1], [2], or [3] for a surprise ')
+        number_of_players = input('\nHow many players? Enter [1], [2], or [3] for a surprise: ')
         self.players = number_of_players
 
     def choose_gesture(self):
-        gesture_list = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
-        print('[0] for Rock')
+        print('\nEnter [0] for Rock')
         sleep(0.2)
-        print('[1] for Paper')
+        print('Enter [1] for Paper')
         sleep(0.2)
-        print('[2] for Scissors')
+        print('Enter [2] for Scissors')
         sleep(0.2)
-        print('[3] for Lizard')
+        print('Enter [3] for Lizard')
         sleep(0.2)
-        print('[4] for Spock')
+        print('Enter [4] for Spock')
         sleep(0.2)
         gesture = input('\nChoose your gesture: ')
         self.gesture = gesture
 
+    def run_game(self):
+        self.display_rules()
+        self.how_many_humans()
+        self.choose_gesture()
+
 test = Game()
-test.display_rules()
-test.choose_gesture()
+test.run_game()
--- player.py
+++ player.py
@@ -1,2 +1,19 @@
+from game import Game
+
 class Player:
-    pass
+    
+    def __init__(self):
+        self.gesture
+
+    def gesture_choice(self, choice):
+        gesture_list = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']
+        if choice == 0:
+            self.gesture = gesture_list[0]
+        elif choice == 1:
+            self.gesture = gesture_list[1]
+        elif choice == 2:
+            self.gesture = gesture_list[2]
+        elif choice == 3:
+            self.gesture = gesture_list[3]
+        elif choice == 4:
+            self.gesture =  gesture_list[4]
